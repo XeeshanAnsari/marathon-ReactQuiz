ef1533befef5fd6d37dcd3cf86bebdaf70b07b4c
xeeshan <xeeshan@gmail.com>
2017-02-05 10:11:11 -0800

    sorting is complete

--- chapter03Project/js/app.js
+++ chapter03Project/js/app.js
@@ -16,7 +16,8 @@
             getInitialState: function(){
            return {
                 data:this.props.initialData,
-                
+                sortby: null,
+                descending: false,
                  };
          },
          propTypes: {
@@ -32,11 +33,15 @@
            _shot: function(e){
                var column = e.target.cellIndex;
                var data = this.state.data.slice();
-               data.sort(function(a,b){
-                   return a[column] > b[column] ;
+               var descending = this.state.sortby  === column  && !this.state.descending;
+                data.sort( function(a,b){
+                   return  descending ? a[column] < b[column]
+                                      : a[column] > b[column] ;
                }) ;
                this.setState({
                    data: data,
+                   sortby:column,
+                   descending: descending,
                });
 
            },
@@ -46,7 +51,9 @@
                      React.DOM.thead({onClick: this._shot},
                        React.DOM.tr(null,
                          this.props.headers.map(function(title , idx) {
-                           
+                           if(this.state.sortby === idx){
+                               title += this.state.descending ? ' \u2191' : ' \u2193'
+                           }
                           return React.DOM.th({key: idx}, title);
                          },this)
                         )
